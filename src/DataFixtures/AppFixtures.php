25fdad5ae7140305fefe6ca8dea7bb43e620d1d0
Allan JARRY <jarry.allan@gmail.com>
2022-02-21 11:45:56 +0100

    [Feat]; add basic authentitication

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Player;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    /**
+     * @param UserPasswordHasherInterface $hasher
+     */
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $player = new Player();
+        $hashedPassword = $this->hasher->hashPassword($player, 'allan');
+        $player
+            ->setUsername('allan.jarry')
+            ->setEmail('allan@drosalys.fr')
+            ->setPassword($hashedPassword)
+        ;
 
+        $manager->persist($player);
         $manager->flush();
     }
 }
